1d5022e0c4b36b819cc68ee87b5d349f56fdcb45
Randy Abreu <randiieh@gmail.com>
2018-01-20 17:18:38 -0400

    first commit

---
+++ README.dm
@@ -0,0 +1,11 @@
+# Genderize test
+
+The tests are written in Node Js and Jest Framework.
+
+Before running the tests install the dependency
+
+```npm install```
+
+To run the tests
+
+```npm test```
---
+++ __test__/genderize.test.js
@@ -0,0 +1,81 @@
+const request = require('request');
+
+function requestPromise(endpoint){
+
+	const api = 'https://api.genderize.io/';
+
+	return new Promise((res,rej) =>{
+
+		request(api + endpoint,(error,response,body) =>{
+
+			if(error){
+				rej(error);
+			} else {
+				res(body);
+			}
+
+		});
+	});
+}
+
+
+describe('Testing the Genderize api',() => {
+
+	it('Test single name endpoint',() =>{
+
+		return requestPromise('?name=peter').then((body) =>{
+
+			const json = JSON.parse(body);
+
+			expect(json).toHaveProperty('name', 'peter');
+
+		});
+
+	});
+
+	it('Test multi name endpoint',() =>{
+
+		const endpoint = '?name[0]=peter&name[1]=lois&name[2]=stevie';
+
+		return requestPromise(endpoint).then((body) =>{
+
+			const json = JSON.parse(body);
+
+			expect(json).toHaveLength(3);
+
+		});
+
+	});
+
+	it('Test probability endpoint',()=>{
+
+		const endpoint = '?name=kim';
+		
+		return requestPromise(endpoint).then((body) =>{
+
+			const json = JSON.parse(body);
+
+			expect(json).toHaveProperty('probability');
+
+			expect(json.probability).toBeGreaterThan(0.5);
+
+		});	
+
+	});
+
+
+	it('Test gender endpoint',() =>{
+
+		const endpoint = '?name=richi';
+
+		return requestPromise(endpoint).then((body) =>{
+
+			const json = JSON.parse(body);
+
+			expect(json).toHaveProperty('gender');
+
+			expect(json.gender).toBe('male');
+		});
+	});
+
+});
